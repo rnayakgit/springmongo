2ee51e1d083d29b1e3a26ac46a721e5b0b67ce5f
RNayakIND <ranjannayak2212gmail.com>
2021-05-16 12:07:37 +0530

    Done

--- src/main/java/com/spring/mongo/application/SpringMongoApplication.java
+++ src/main/java/com/spring/mongo/application/SpringMongoApplication.java
@@ -1,5 +1,4 @@
 package com.spring.mongo.application;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
